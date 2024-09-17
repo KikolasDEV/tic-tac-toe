d36941f4052a1bd1c6ad8328591e70f67e1a4f5f
KikolasDEV <kikoamarillo94@gmail.com>
2024-09-17 13:14:39 +0200

    Startup Project

---
+++ main.py
@@ -0,0 +1,7 @@
+'''
+Using what you have learnt about Python programming, you will build a text-based version of the Tic Tac Toe game.
+The game should be playable in the command line.
+It should be a 2-player game, where one person is "X" and the other plays "O".
+This is a simple demonstration of how the game works: https://www.google.com/search?q=tic+tac+toe
+AI player to play the game with you?
+'''
